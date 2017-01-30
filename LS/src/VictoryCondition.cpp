5ede5d7497fae71f0c7662b50572469d6b985b2d
Grimwan <deankulenovic@gmail.com>
2017-01-30 13:34:03 +0100

    Added victory condition/still working on it
    still working on it

---
+++ LS/src/VictoryCondition.cpp
@@ -0,0 +1,20 @@
+#include "VictoryCondition.h"
+
+Victory::Victory()
+{
+	_gotTheTreasure = false;
+	_atTheExit = false;
+	Grid gettingTheGrid;
+
+
+}
+
+Victory::~Victory()
+{
+
+}
+
+void Victory::checkifPlayerWon(glm::vec3 playerpos)
+{
+
+}
