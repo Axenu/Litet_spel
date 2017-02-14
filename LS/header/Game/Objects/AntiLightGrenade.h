7c5e26277a580831530f147a1cb297a100bdfff0
Grimwan <deankulenovic@gmail.com>
2017-02-14 22:06:49 +0100

    AntiLightGrenade in work
    AntiLightGrenade

---
+++ LS/header/Game/Objects/AntiLightGrenade.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "gl/glInclude.h"
+#include "GameObject.h"
+#include "GridDataStructure.h"
+#include "Event/Input/InputManager.h"
+#include "Event/EventManager.h"
+#include "Character.h"
+#include <stdlib.h>
+#include <time.h>
+#include "intersectionFunctions.h"
+
+class AntiLightGrenade : public GameObject
+{
+private:
+
+
+public:
+AntiLightGrenade();
+~AntiLightGrenade();
+};
---
+++ LS/src/Game/Objects/AntiLightGrenade.cpp
@@ -0,0 +1,12 @@
+#include "Game/Objects/AntiLightGrenade.h"
+
+AntiLightGrenade::AntiLightGrenade()
+{
+
+}
+
+AntiLightGrenade::~AntiLightGrenade()
+{
+
+}
+
