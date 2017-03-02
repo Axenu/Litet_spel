38a2900275edb1a05553c53ecb31222c2fa94e0d
LITTLEONE\Mattias <mattias-fredriksson91@hotmail.se>
2017-03-02 22:02:59 +0100

    Nya färger

--- LS/src/Game/Level/Level.cpp
+++ LS/src/Game/Level/Level.cpp
@@ -11,22 +11,34 @@ Level::Level(const std::string &level, EventManager *eventManager, MeshShader &s
 
 	Material floorMat(&shader);
 	floorMat.setColor("diffuse", glm::vec4(0.1f));
+	floorMat.setColor("spec", glm::vec4(0.2f));
+	floorMat.setFloat("shine", 5.f);
+
+	Material wallMat(&shader);
+	wallMat.setColor("diffuse", glm::vec4(0.2f, 0.01f, 0.005f, 1.0f));
+	wallMat.setColor("spec", glm::vec4(0.5f));
+	wallMat.setFloat("shine", 20.f);
+	/* Old mats
+	Material floorMat(&shader);
+	floorMat.setColor("diffuse", glm::vec4(0.1f));
 	floorMat.setColor("spec", glm::vec4(0.8f, 0.2f, 0.05f, 1.0f));
 	floorMat.setFloat("shine", 20.f);
-	ModelPart floorPart(&_gridMesh[0], floorMat);
-	parts.push_back(floorPart);
 
 	Material roofMat(&shader);
 	roofMat.setColor("diffuse", glm::vec4(1.f));
 	roofMat.setColor("spec", glm::vec4(0.2f));
 	roofMat.setFloat("shine", 20.f);
-	ModelPart roofPart(&_gridMesh[1], roofMat);
-	parts.push_back(roofPart);
 
 	Material wallMat(&shader);
 	wallMat.setColor("diffuse", glm::vec4(0.8f));
 	wallMat.setColor("spec", glm::vec4(0.2f));
 	wallMat.setFloat("shine", 20.f);
+	*/
+	//Create meshes
+	ModelPart floorPart(&_gridMesh[0], floorMat);
+	parts.push_back(floorPart);
+	ModelPart roofPart(&_gridMesh[1], floorMat);
+	parts.push_back(roofPart);
 	ModelPart wallPart(&_gridMesh[2], wallMat);
 	parts.push_back(wallPart);
 
