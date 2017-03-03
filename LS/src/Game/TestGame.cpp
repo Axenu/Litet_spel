f35eca11a7cd05225a9d3f851a4074a0c4128224
LITTLEONE\Mattias <mattias-fredriksson91@hotmail.se>
2017-03-03 15:32:08 +0100

    Missed a file

--- LS/src/Game/TestGame.cpp
+++ LS/src/Game/TestGame.cpp
@@ -11,29 +11,16 @@ TestGame::~TestGame()
 	std::cout << "delete game" << std::endl;
 }
 
-void TestGame::setupRI(RenderInfo &rI) {
-	rI._arraysize = _player->amountOfGrenades();
-	rI._grenadeID = _player->getGrenadeID();
-	for (int i = 0;i< _player->amountOfGrenades();i++)
-		{
-		rI._lightGrenadePos[i] = _player->getGrenadeData()[i]._grenadePositionWhenLanded;
-		rI._lightGrenadeExpansionAndFading[i] = glm::vec2(_player->getGrenadeData()[i].expanding, _player->getGrenadeData()[i].fading);
-	}
-}
-void TestGame::initiate() {
+std::unique_ptr<Scene> TestGame::spawnScene() {
 
 	//Create the scene, and the level
 	Level* level;
-	_scene = _factory.createLevel("Demo1.bmp", level);
+	std::unique_ptr<Scene> scene = _factory.createLevel("Demo1.bmp", level);
 	//Initiate camera and renderer vars:
-	Camera& cam = _scene->setCamera(_setting);
+	Camera& cam = scene->setCamera(_setting);
 	_deferred.setWindowSize((float)_setting.Width(), (float)_setting.Height(), cam);
-	_bufferRenderer.setWindowSize((float)_setting.Width(), (float)_setting.Height(), _scene->getCamera());
-	int _amountOfGrenades = GrenadeAmountFromCharacter;
-	std::vector<AntiLightGrenade*> grenade;
-	for(int i = 0;i<_amountOfGrenades;i++)
-	 grenade.push_back(_factory.createAntiLightGrenade("grenade.obj", glm::ivec2(2, 2)));
-	Character* player = _factory.createCharacter(glm::ivec2(3, 5), 1.3f, grenade);
+	_bufferRenderer.setWindowSize((float)_setting.Width(), (float)_setting.Height(), scene->getCamera());
+	Character* player = _factory.createCharacter(glm::ivec2(3, 5), 1.3f);
 	_player = player;
 
 
@@ -53,6 +40,11 @@ void TestGame::initiate() {
 	for (unsigned int i = 0; i < guards.size(); i++)
 		_factory.createGuard("Character.dae", guards[i].spawnPosition, *player, guards[i].walkingPoints);
 	_factory.createRandomLoot(loot, 20.f);
+
+	/* Preload
+	*/
+	_factory.preLoadModel("grenade.obj");
+	return scene;
 }
 
 
